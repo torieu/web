b7c79d298b62fbc7ad8fce257148ac420c2387f0
torieu <103324898+torieu@users.noreply.github.com>
2024-10-10 14:35:57 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
     <!-- Conditionally render the header -->
-<nav v-if="showHeader" style="display: flex; align-items: center; height: 100%;">
-  <ul style="width: 100%; margin: 0; padding: 0;">
-    <li class="handwritten" style="font-size: 1.5em; list-style-type: none;">
-      <a @click="goBack" style="display: flex; align-items: center; cursor: pointer;">
-        <img style="width: 1.5vw; margin-right: 5px;" :src="pin" alt="red pin" />
-        nazpět.
-      </a>
-    </li>
-  </ul>
-</nav>
+    <nav v-if="showHeader" style="display: flex; align-items: center; height: 100%;">
+      <ul style="width: 100%; margin: 0; padding: 0; display: flex; justify-content: space-between; align-items: center;">
+        <li class="handwritten" style="font-size: 1.5em; list-style-type: none;">
+          <a @click="goBack" style="display: flex; align-items: center; cursor: pointer;">
+            nazpět.
+          </a>
+        </li>
+        <li style="display: flex; gap: 10px; align-items: center;">
+          <router-link to="/" class="nav-pin">
+            <img :src="pin" alt="hlavní stránka" style="width: 1.5vw;" />
+          </router-link>
+          <router-link to="/about" class="nav-post-it">
+            <img :src="yellowPostit" alt="kdo jsem" style="width: 2vw;" />
+          </router-link>
+          <router-link to="/KISK" class="nav-post-it">
+            <img :src="pinkPostit" alt="KISK" style="width: 2vw;" />
+          </router-link>
+          <router-link to="/matematika" class="nav-post-it">
+            <img :src="greenPostit" alt="matika" style="width: 2vw;" />
+          </router-link>
+        </li>
+      </ul>
+    </nav>
 
     <!-- Render the router view -->
     <router-view />
@@ -21,6 +34,9 @@
 import { computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import pin from './assets/red-pin.svg';
+import yellowPostit from './assets/postit-yellow.svg';
+import pinkPostit from './assets/postit-pink.svg';
+import greenPostit from './assets/postit-green.svg';
 
 export default {
   name: 'App',
@@ -45,8 +61,25 @@ export default {
   data() {
     return {
       pin,
+      yellowPostit,
+      pinkPostit,
+      greenPostit,
     };
   },
 };
 </script>
+
+<style scoped>
+.nav-post-it, .nav-pin {
+  transition: transform 0.2s ease-in-out;
+}
+
+.nav-post-it:hover, .nav-pin:hover {
+  transform: scale(1.1);
+}
+
+.nav-pin {
+  margin-right: 5px;
+}
+</style>
 
